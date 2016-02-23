ca79717bf496ee1fbffe7ff748edea2a11e8ac5b
Phillip Fry <phillip_fry@umail.ucsb.edu>
2016-02-23 07:09:30 -0800

    Added a unit conversion test Decimal to Binary

--- app/src/main/java/com/example/henry/mycalculator/Main3Activity.java
+++ app/src/main/java/com/example/henry/mycalculator/Main3Activity.java
@@ -8,28 +8,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.RadioButton;
 
 //import com.google.android.gms.appindexing.Action;
 //import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class Main3Activity extends Activity implements OnClickListener {
 
-   /* Button btnStartAnotherActivity2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+   // private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-
-        btnStartAnotherActivity2 = (Button) findViewById(R.id.btnStartAnotherActivity);
-
-        btnStartAnotherActivity2.setOnClickListener(this);
-    }*/
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -41,4 +42,77 @@ public class Main3Activity extends Activity implements OnClickListener {
         startActivity(intent);
     }
 
+    public void ButtonStartQuiz(View v) {
+        if (v.getId() == R.id.btnStartQuizActivity) {
+            Intent i = new Intent(Main3Activity.this, Main4Activityquiz.class);
+            startActivity(i);
+        }
+    }
+
+    public void Qnum(View view) {
+        // is the button checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        //Which radio button clicked?
+        switch (view.getId()) {
+            case R.id.SixQs:
+                if (checked)
+                    // 6 questions
+                    break;
+            case R.id.TwelveQs:
+                if(checked)
+                    // 12 questions
+                break;
+            case R.id.twenty4Qs:
+                if(checked)
+                    // 24 questions
+                break;
+            case R.id.Endless:
+                if(checked)
+                    // Endless
+                break;
+        }
+
+    }
+
+   /* @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main3 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.henry.mycalculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main3 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.henry.mycalculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+*/
 }
--- app/src/main/java/com/example/henry/mycalculator/Main4Activityquiz.java
+++ app/src/main/java/com/example/henry/mycalculator/Main4Activityquiz.java
@@ -4,19 +4,53 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Main4Activityquiz extends Activity implements OnClickListener {
+import org.w3c.dom.Text;
 
+import java.util.Random;
+
+public class Main4Activityquiz extends Activity implements OnClickListener{
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4_activityquiz);
+    }
+
+
     public void onClick(View view) {
         Intent intent = new Intent(this, Main4Activityquiz.class);
 
         // Calling another activity Main4Activitytest start the quiz
 
         startActivity(intent);
+    }
+
+
+    String myString = "0";
+    public void TestResult(View view){
+        int binary = Integer.parseInt(myString,2);
+        EditText answer = (EditText) findViewById(R.id.AnswerField);
+        TextView respond = (TextView) findViewById(R.id.RightOrWrong);
+        int binaryanswer = Integer.parseInt(answer.getText().toString());
+        if(binary == binaryanswer){
+            respond.setText("Correct");
+        } else {
+            respond.setText("Incorrect");
+        }
+
+        Random rand = new Random();
+        int number = rand.nextInt(4097);
+        TextView myText = (TextView)findViewById(R.id.decimal);
+        String myString = String.valueOf(number);
+        myText.setText(myString);
     }
 
 
